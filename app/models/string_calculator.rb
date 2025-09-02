06292e71b07c7aa588395f66e87daeef8ae31a06
Akash Kumrawat <akash.kumrawat.sdbg@gmail.com>
2025-09-02 14:05:34 +0530

    Handled any number of argument delimiter and negative number

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -2,6 +2,35 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
     
-    numbers.split(',').map(&:to_i).sum
+    delimiter, numbers = extract_delimiter_and_numbers(numbers)
+    number_array = parse_numbers(numbers, delimiter)
+    validate_no_negative_numbers(number_array)
+    
+    number_array.sum
+  end
+
+  private
+
+  def extract_delimiter_and_numbers(input)
+    if input.start_with?("//")
+      delimiter_line, numbers = input.split("\n", 2)
+      custom_delimiter = delimiter_line[2..-1]
+      delimiter = /[,\n#{Regexp.escape(custom_delimiter)}]/
+      [delimiter, numbers]
+    else
+      [/[,\n]/, input]
+    end
+  end
+
+  def parse_numbers(numbers, delimiter)
+    numbers.split(delimiter).map(&:to_i)
+  end
+
+  def validate_no_negative_numbers(number_array)
+    negative_numbers = number_array.select { |n| n < 0 }
+    
+    if negative_numbers.any?
+      raise ArgumentError, "negative numbers not allowed #{negative_numbers.join(',')}"
+    end
   end
 end
--- test/models/string_calculator_test.rb
+++ test/models/string_calculator_test.rb
@@ -18,4 +18,40 @@ class StringCalculatorTest < ActiveSupport::TestCase
     assert_equal 6, @calculator.add("1,5")
     assert_equal 3, @calculator.add("1,2")
   end
+
+  test "handles any amount of numbers" do
+    assert_equal 10, @calculator.add("1,2,3,4")
+    assert_equal 15, @calculator.add("1,2,3,4,5")
+    assert_equal 6, @calculator.add("1,2,3")
+  end
+
+  test "supports newlines as delimiters" do
+    assert_equal 6, @calculator.add("1\n2,3")
+    assert_equal 6, @calculator.add("1\n2\n3")
+    assert_equal 10, @calculator.add("1,2\n3,4")
+  end
+
+  test "supports custom delimiters" do
+    assert_equal 3, @calculator.add("//;\n1;2")
+    assert_equal 6, @calculator.add("//|\n1|2|3")
+    assert_equal 10, @calculator.add("//*\n1*2*3*4")
+  end
+
+  test "throws exception for single negative number" do
+    exception = assert_raises(ArgumentError) { @calculator.add("1,-2,3") }
+    assert_equal "negative numbers not allowed -2", exception.message
+  end
+
+  test "throws exception for multiple negative numbers" do
+    exception = assert_raises(ArgumentError) { @calculator.add("1,-2,3,-4") }
+    assert_equal "negative numbers not allowed -2,-4", exception.message
+  end
+
+  test "comprehensive test with custom delimiter and mixed scenarios" do
+    assert_equal 6, @calculator.add("//;\n1;2;3")
+    assert_equal 0, @calculator.add("")
+    assert_equal 1, @calculator.add("1")
+    assert_equal 15, @calculator.add("1,2,3,4,5")
+    assert_equal 6, @calculator.add("1\n2,3")
+  end
 end
